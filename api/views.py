8c45b417ad5b588e488de9e7af3daa4ddde8bf90
Simon Archer <simon.james.archer@gmail.com>
2021-09-19 13:45:25 +0100

    Adding first view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def main(request):
+    return JsonResponse({'status': 200, 'content': 'Succesfully connected to backend'})
--- cans_backend/urls.py
+++ cans_backend/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import api
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(api.urls))
+    path('api/', include('api.urls'))
 ]
