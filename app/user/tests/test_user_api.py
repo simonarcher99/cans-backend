9083f228888ff89fee69f6a747f3a1bf7d74d487
Simon Archer <simon.james.archer@gmail.com>
2021-10-21 15:54:09 +0100

    Adding password above 5 characters validation

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -46,3 +46,19 @@ class PublicUserTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test creating a user with a password 5 characters or less fails"""
+        payload = {
+            'email': 'test@test.com',
+            'password': 'pass',
+            'name': 'Test'
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        )
+        self.assertFalse(user_exists)
