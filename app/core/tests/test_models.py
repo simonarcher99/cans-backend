5588648c5999ddd84dcb4db67ef1e76f833aca3a
Simon Archer <simon.james.archer@gmail.com>
2021-10-21 14:23:40 +0100

    Adding test to test that email is normalized

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,10 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test that the email for a new user is normalized"""
+        email = "test@TEST.com"
+        user = get_user_model().objects.create_user(email, 'pass123')
+
+        self.assertEqual(user.email, email.lower())
